07a4d2558856740a14697fd4426da6a329ce9237
SahilShah518 <sahilshah379@gmail.com>
2019-07-28 18:05:58 -0400

    uhh

--- ParachuteReefing/ReefingModel.py
+++ ParachuteReefing/ReefingModel.py
@@ -5,16 +5,19 @@ m = 650/1000 # mass
 g = 9.80665 # gravity
 time = 32
 height = 245
-deltaTime = 1
+deltaTime = 1 # time between each update
 
 def main():
 	t = time # time remaining
 	h = height # altitude
 	vi = 0
 	k = 0
+	kPrev = 0
 	increase = True
 	print('(t,h,vi,k): (%s,%s,%s,%s)' %(t,h,vi,k))
 	while (t > 2):
+		h = h # use altimeter data
+		
 		if vi*t <= h:
 			h1 = h-(vi*t)
 			increase = True
@@ -31,17 +34,28 @@ def main():
 		else:
 			termVelOverall = vi-termVelPart
 
-		k = m*g/pow(termVelOverall,2)
-
+		k = m*g/pow(termVelOverall,2) # change the servo to match this k value
 		if (increase):
 			vi += math.sqrt(m*g/kPart)*math.tanh(math.sqrt(g*kPart/m)*deltaTime)
-			h -= ((vi*deltaTime)+(m/kPart)*math.log(math.cosh(math.sqrt(g*kPart/m)*deltaTime)))
 		else:
 			vi -= math.sqrt(m*g/kPart)*math.tanh(math.sqrt(g*kPart/m)*deltaTime)
-			h -= ((vi*deltaTime)-(m/kPart)*math.log(math.cosh(math.sqrt(g*kPart/m)*deltaTime)))
+		
+		# beginning of model specific calculations
+		if (abs(h1) > 0.01):
+			hPart = None
+			try:
+				hPart = (m/kPart)*math.log(math.cosh(math.sqrt(g*kPart/m)*deltaTime))
+			except:
+				hPart = 0
+			if (increase):
+				deltaH = (vi*deltaTime)+hPart
+			else:
+				deltaH = (vi*deltaTime)-hPart
+			deltaH *= (random.randint(95,105)/100)
+			h -= deltaH
+		# end of model specific calculations
 
 		t -= deltaTime
-		# h *= (random.randint(95,105)/100)
 		print('(t,h,vi,k): (%s,%s,%s,%s)' %(t,h,vi,k))
 
 
