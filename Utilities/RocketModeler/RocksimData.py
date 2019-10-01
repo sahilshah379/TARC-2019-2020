79f6698601e398f1bdfc2c1ff97094a03d03e29e
SahilShah518 <sahilshah379@gmail.com>
2019-10-01 16:59:23 -0400

    read data

---
+++ Utilities/RocketModeler/RocksimData.py
@@ -0,0 +1,19 @@
+import csv
+
+flight_data = []
+
+def main():
+	readCSV()
+	altitude = flight_data[[i for i in range(len(flight_data)) if flight_data[i]['Time'] == 0.105][0]]['Altitude Feet']
+	print(altitude)
+
+def readCSV():
+	with open('TARC-2019-2020.csv') as file:
+		read = csv.DictReader(file, delimiter=',')
+		for row in read:
+			raw_data = dict(row)
+			data = {k : float(v.replace(' ','')) for k,v in raw_data.items()}
+			flight_data.append(data)
+
+if __name__ == '__main__':
+	main()
