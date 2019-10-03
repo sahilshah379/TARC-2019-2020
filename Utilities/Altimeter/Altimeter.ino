e556bd9e60fbabe7ccef647e9f1ad629cabb9782
SahilShah518 <sahilshah379@gmail.com>
2019-10-03 16:47:43 -0400

    aaahh

--- Utilities/Altimeter/Altimeter.ino
+++ Utilities/Altimeter/Altimeter.ino
@@ -1,9 +1,13 @@
 #include <Arduino_LPS22HB.h> // pressure
 #include <Arduino_HTS221.h> // humidity & temperature
 
-const seaLevelPressure = 1013; // https://weather.us/observations/air-pressure-station.html
+const float seaLevelPressure = 1019; // https://weather.us/observations/pressure-qnh.html
+float prevAlt = 0;
+float prevTime = 0;
 
 void setup() {
+  Serial.begin(9600);
+  while (!Serial);
   if (!BARO.begin()) {
       Serial.println("Failed to initialize pressure sensor!");
       while (1);
@@ -12,11 +16,20 @@ void setup() {
       Serial.println("Failed to initialize humidity temperature sensor!");
       while (1);
   }
+  prevTime = millis();
 }
 
 void loop() {
   float temperature = HTS.readTemperature(); // temperature (celsius)
   float pressure = BARO.readPressure() * 10; // pressure (hPa)
-  float altitude = ((pow((seaLevelPressure/pressure),(1/5.257))-1)*(temperature+273.15))/0.0065
-  print(altitude)
+  float altitude = ((pow((seaLevelPressure/pressure),(1/5.257))-1)*(temperature+273.15))/0.0065;
+  Serial.print("Altitude: ");
+  Serial.println(altitude);
+  float velocity = 1000*(altitude-prevAlt)/((millis()-prevTime));
+  Serial.print("Velocity: ");
+  Serial.println(velocity);
+  prevAlt = altitude;
+  prevTime = millis();
+  Serial.println();
+  delay(100);
 }
