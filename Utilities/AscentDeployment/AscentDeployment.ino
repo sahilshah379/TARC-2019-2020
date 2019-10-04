01c97dfa6060e53a6ea9c28708e6d7579cc2fbe2
zeroman29 <38667592+zeroman29@users.noreply.github.com>
2019-10-04 15:23:45 -0400

    Update AscentDeployment.ino

--- Utilities/AscentDeployment/AscentDeployment.ino
+++ Utilities/AscentDeployment/AscentDeployment.ino
@@ -27,6 +27,20 @@ void setup() {
 void loop() {
   float altitude = getAltitude();
   float velocity = getVelocity(altitude);
+  if (altitude + getMaxAltitude() >= apogee/3.281) {
+    Serial.println("Deploying parachute");
+    Serial.print("Time: ");
+    Serial.println(prevTime/1000);
+    Serial.print("Altitude: ");
+    Serial.println(altitude);
+    Serial.print("Will travel ");
+    Serial.print(getMaxAltitude());
+    Serial.println(" m after deployment");
+    Serial.print("Apogee at ");
+    Serial.println(altitude+getMaxAltitude());
+    while(1) {
+    }
+  }
   delay(100);
 }
 
@@ -35,6 +49,11 @@ float getAltitude() {
   float pressure = BARO.readPressure() * 10; // pressure (hPa)
   float altitude = ((pow((seaLevelPressure/pressure),(1/5.257))-1)*(temperature+273.15))/0.0065;
   return altitude;
+}
+float getMaxAltitude() {
+	float vt = sqrt(mass*gravity/parachuteK);
+  yMax = (pow(vt,2)/(2*gravity))*math.log((pow(velocity,2)+pow(vt,2))/(pow(vt,2)));
+	return yMax;
 }
 float getVelocity(float altitude) {
   float velocity = 1000*(altitude-prevAlt)/((millis()-prevTime));
