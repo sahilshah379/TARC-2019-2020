282bf1ce33edfae1d5aa53f88461e0807e355df6
Sahil Shah <sahilshah379@gmail.com>
2019-10-28 16:39:49 -0400

    oye

--- Utilities/SeparationServo/SeparationServo.ino
+++ Utilities/SeparationServo/SeparationServo.ino
@@ -1,25 +1,34 @@
 #define pin 10
 
 const int refresh_interval = 3333;
-const int pulseLower = 750;
-const int pulseUpper = 2250;
-const int maxDegrees = 145;
+const int pulse_lower = 750;
+const int pulse_upper = 2250;
+const int max_degrees = 145;
+const int close_pulse = 1400;
+const int open_pulse = 1000;
 
 int pos = 0;
-int pulse = 1500;
 
 void setup() {
-    pinMode(pin, OUTPUT);
+  pinMode(pin, OUTPUT);
 }
 
 void loop() {
-    for (int i = 0; i < 100; i += 1) {
+    for (int i = 0; i < 120; i += 1) {
         digitalWrite(pin, HIGH);
-        delayMicroseconds(pulse);
+        delayMicroseconds(close_pulse);
         digitalWrite(pin, LOW);
-        delayMicroseconds(refresh_interval-pulse);
+        delayMicroseconds(refresh_interval-close_pulse );
     }
+    delay(1000);
+    for (int i = 0; i < 120; i += 1) {
+        digitalWrite(pin, HIGH);
+        delayMicroseconds(open_pulse);
+        digitalWrite(pin, LOW);
+        delayMicroseconds(refresh_interval-open_pulse);
+    }
+    delay(1000);
 }
 int degreesToPulse(int degrees) {
-    return map(degrees, 0, 145, pulseLower, pulseUpper);
+    return map(degrees, 0, max_degrees, pulse_lower, pulse_upper);
 }
